2065f137c16d7e5c176f2b5bd03b9e8dff103021
KonuhovAND <andreykonuhov8@gmail.com>
2025-09-05 21:45:54 +0300

    complete second task

---
+++ Laba1/task2.py
@@ -0,0 +1,23 @@
+# Напечатайте все трехзначные числа с заданной
+# пользователем суммой цифр
+
+from itertools import permutations
+
+
+def generate_numeber(number):
+    for num in range(100, 1000):
+        if number == (num % 10 + num // 10 - 9 * (num // 100)):
+            print(num, end=" ")
+
+
+def main():
+    try:
+        number = int(input("Enter number in range 1-27: "))
+        if number < 1 or number > 27:
+            raise ValueError("Enter valid number!")
+        generate_numeber(number)
+    except Exception as exc:
+        print(exc, f"{number} is not in range [1,27]")
+
+
+main()
