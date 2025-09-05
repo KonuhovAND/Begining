ac361774d33e2fe3b639183e0e70dfd46798f02f
KonuhovAND <andreykonuhov8@gmail.com>
2025-09-05 21:20:21 +0300

    first task, first laba

---
+++ Laba1/task1.py
@@ -0,0 +1,39 @@
+#  Пользователь вводит с консоли три числа: размер
+# вклада, процентную ставку и количество лет, на которое
+# рассчитан вклад. Выведите на консоль таблицу в формате «год
+# - сумма», которая отображала бы рост вклада по годам.
+
+
+def is_item_less_than_zero(item, name):
+    if item <= 0:
+        raise ValueError(
+            f"{name.capitalize()} is less than 0, enter positive {name}")
+
+
+def main():
+    try:
+        print("3.5 years = 3 years")
+        print("4.93 years = 4 years")
+
+        value = float(
+            input("Enter amount of money u want to put(more than 0): "))
+        is_item_less_than_zero(value, "value")
+
+        percent = float(input("Enter percent(more than 0): "))
+        is_item_less_than_zero(percent, "percent")
+
+        years = int(
+            float(input("Enter how long u want to hold money(more than 0): ")))
+        is_item_less_than_zero(years, "years")
+
+        print(f"{'YEARS':<10}{'VALUE':<5}")
+
+        for i in range(1, years + 1):
+            value *= 1 + (percent / 100)
+            print(f"{i:^5}{round(value, 2):<5}")
+
+    except Exception as exc:
+        print(exc)
+
+
+main()
