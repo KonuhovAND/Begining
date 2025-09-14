7d3261759dc9a622b93664c20d67605859119d04
KonuhovAND <andreykonuhov8@gmail.com>
2025-09-14 13:16:15 +0300

    make seconde task

--- Laba2/task2.py
+++ Laba2/task2.py
@@ -15,15 +15,28 @@
 # Возможно, для решения этой задачи вам понадобиться изучить
 # модуль статистик
 import statistics as st
+import re
 
-
-def stats_calculator(*args, mode=""):
-    if mode == "basic":
-        print(f"Max - {max(*args)}, min - {min(*args)}, avg - {st.mean(*args)}")
-    elif mode == "advanced":
-        print(f"Max - {max(*args)}, min - {min(*args)}, avg - {st.mean(*args)}")
-        print(f"Median - {st.median(*args)},moda - {st.mode(*args)}")
-    elif mode == "scientific":
-        print(f"Max - {max(*args)}, min - {min(*args)}, avg - {st.mean(*args)}")
-        print(f"Median - {st.median(*args)},moda - {st.mode(*args)}")
-        print(f"")
+def stats_calculator(*args, mode):
+    if mode == "bs":
+        print(f"Max - {max(args)}, min - {min(args)}, avg - {st.mean(args)}")
+    elif mode == "ad":
+        print(f"Max - {max(*args)}, min - {min(*args)}, avg - {st.mean(args)}")
+        print(f"Median - {st.median(args)},moda - {st.mode(args)}")
+    elif mode == "sc":
+        print(f"Max - {max(*args)}, min - {min(*args)}, avg - {st.mean(args)}")
+        print(f"Median - {st.median(args)},moda - {st.mode(args)}")
+        print(f"Avg geom - {st.geometric_mean(args)}, avg harmonic - {st.harmonic_mean(args)}")
+def main():
+    try:
+        data = list(map(float,(input('Enter data to analyze: ')).split() ))
+    except ValueError as verr:
+        print(f"{verr}\nPlese,enter valid data [1,1.5,12]")
+        data = list(map(float,(input('Enter data to analyze: ')).split() ))
+    try: 
+        mode = input("""Enter mode to operations \n1)basic(bs)\n2)advanced (ad)\n3)scientific(sc): """)
+    except ValueError as verr:
+        print(f"{verr}\n Please correct mode")
+        mode = str(input(r"""Enter mode to operations 1)basic(bs)\n2)advanced (ad)\n3)scientific(sc)\n: """))
+    stats_calculator(*data,mode=mode)
+main()
