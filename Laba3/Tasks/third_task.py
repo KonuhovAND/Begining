26c579ad48f831f7999cd8e5549ffc04914f1c5c
KonuhovAND <andreykonuhov8@gmail.com>
2025-10-01 11:13:08 +0300

    change algoritm how to calculate number in шифре  цезаря

--- Laba3/Tasks/third_task.py
+++ Laba3/Tasks/third_task.py
@@ -5,7 +5,7 @@ result = ""
 for char in text:
     if char.isalpha():
         start = ord('A') if char.isupper() else ord('a')
-        shifted = (ord(char) - start + k) % 26 - start
+        shifted = (ord(char) - start + k) % 26 + start
         result += chr(shifted)
     else:
         result += char
