e7d4c0a8e427f79fa8d7ebab0d5de8ed09c20013
KonuhovAND <andreykonuhov8@gmail.com>
2025-10-01 11:19:43 +0300

    feat: make fibanachi func

--- Laba3/Tasks/second_task.py
+++ Laba3/Tasks/second_task.py
@@ -1,8 +1,12 @@
+
+
 n = int(input("Введите количество чисел Фибоначчи: "))
 
-fib = [0,1, 1]
-for i in range(2, n):
-    fib.append(fib[i] + fib[i - 1])
+def fibanachi_numbers(n):
+    fib = [0,1, 1]
+    for i in range(2, n):
+        fib.append(fib[i] + fib[i - 1])
 
-print("Первые", n, "чисел Фибоначчи:")
-print(fib[:n])
+    print("Первые", n, "(чисел Фибоначчи:")
+    print(fib[:n])
+fibanachi_numbers(n=n)
