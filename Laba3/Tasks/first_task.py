40f3d9d09fc8f2f69f4001119922e075adcd7651
KonuhovAND <andreykonuhov8@gmail.com>
2025-10-01 11:17:42 +0300

    fix: change essential things in code

--- Laba3/Tasks/first_task.py
+++ Laba3/Tasks/first_task.py
@@ -4,7 +4,7 @@ if len(ticket) == 6:
     first_half = sum(int(digit) for digit in ticket[:3])
     second_half = sum(int(digit) for digit in ticket[3:])
     if first_half == second_half:
-        print("Счастливый билет!")
+        print("Счастливый билет! ")
     else:
         print("Обычный билет.")
 else:
